7bc6ba1bf0f8e36a5328410984ffd159ffbcf98f
Robin <robin.saberinasseri@gmail.com>
2021-08-17 18:30:31 +0200

    wip version

--- pyscripts/context_window.py
+++ pyscripts/context_window.py
@@ -1,17 +1,12 @@
+from typing import Counter
 import numpy as np
 import pandas as pd
 from collections import deque
 import os,json
 import re
 from progressbar import progressbar
-from utils import paragraph_iterator
+from pyparlaclarin.read import speeches_with_name
 from lxml import etree
-from itertools import chain
-
-# Constructs dataset for word sense induction
-# Requires pyriksdagen folders in cd
-# Should change generator, can get documents like "interpellation om forskning undervisning och information på näringslärans område" multiple times
-# Only slight preprocessing of text is implemented atm
 
 # Target words
 target = ['Information', 'information', 'informations', 'informationen', 'informationens', 'informationer', 'informationers', 'informationerna', 'informationernas',\
@@ -19,7 +14,7 @@ target = ['Information', 'information', 'informations', 'informationen', 'inform
 'Propaganda', 'propaganda', 'propagandas', 'propagandan', 'propagandans']
 
 # Context window size (on each side of target word)
-window = 25
+window = 50
 data_list = []
 
 # Take subset for debugging (set to 0 for all files)
@@ -28,84 +23,97 @@ c = 0
 
 riksdagen_path = '/home/robinsaberi/Git_Repos/riksdagen-corpus/corpus'
 
+# Extract number of folders
+n_folders = 0
+for path, subdirs, files in os.walk(riksdagen_path):
+    for name in subdirs:
+        n_folders += 1
+
 # Traverse corpus folder and subdirectories
-#for root, dirs, files in os.walk("corpus/"):
-for root, dirs, files in os.walk(riksdagen_path):
-    path = root.split(os.sep)
-    folder = os.path.basename(root)
-    if folder.isnumeric():
-        for file in progressbar(files):
-            file_path = '/'.join([riksdagen_path,folder,file])
-            parser = etree.XMLParser(remove_blank_text=True)
-            root = etree.parse(file_path, parser).getroot()
-
-            # Create generator for paragraphs within xml file
-            p_iter = paragraph_iterator(root)
-
-            for p in p_iter:
-                # Process string
-                p = re.sub(r'[^A-Za-z0-9À-ÿ /-]+', '', p) # Remove special characters
-                p = p.lower()
-                txt = deque(p.split()) # Not sure if needed, but easy way to pad beginning of list
-
-                # Pad documents in case context window goes outside of paragraph
-                txt.extendleft(['']*window)
-                txt.extend(['']*window)
-                txt = list(txt)
-
-                # Find target word indices
-                pos = [i for i,word in enumerate(txt) if word in target]
-
-                # Retrive pseudo-documents
-                for i in pos:
-                    context = txt[(i-window):(i+window+1)]
-                    context.append(file_path)
-                    data_list.append(context)
-
-        print(f'Folder {folder} finished')
-
-        # For debugging
-        c += 1
-        if c == max_folders:
-            break
-
-# Write as df for exploration
-df = pd.DataFrame(data_list)
-df.rename(columns={ df.columns[-1]: "file" }, inplace = True)
-df.to_csv("test_context_window_data.csv", index=False)
+#for root, dirs, files in os.walk(riksdagen_path):
+#    path = root.split(os.sep)
+#    folder = os.path.basename(root)
+#    if folder.isnumeric():
+#        for file in progressbar(files):
+#            file_path = '/'.join([riksdagen_path,folder,file])
+#            parser = etree.XMLParser(remove_blank_text=True)
+#            root = etree.parse(file_path, parser).getroot()
+#
+#            # Iterate over speeches within xml file
+#            for speech in speeches_with_name(root):
+#
+#                # Process string
+#                speech = re.sub(r'[^A-Za-z0-9À-ÿ /-]+', '', speech) # Remove special characters
+#                speech = speech.lower()
+#                txt = deque(speech.split()) # Not sure if needed, but easy way to pad beginning of list
+#
+#                # Pad documents in case context window goes outside of paragraph
+#                txt.extendleft(['']*window)
+#                txt.extend(['']*window)
+#                txt = list(txt)
+#
+#                # Find target word indices
+#                pos = [i for i,word in enumerate(txt) if word in target]
+#
+#                # Retrieve pseudo-documents
+#                for i in pos:
+#                    context = txt[(i-window):(i+window+1)]
+#                    context.append('/'.join([folder, file]))
+#                    data_list.append(context)
+#
+#        # Overall progress
+#        c += 1
+#        if c % 5 == 0:
+#            print(f'{round(c / n_folders * 100, 2)}% of folders finished')
+#        
+#        # For debugging
+#        if c == max_folders:
+#            break
+#        
+## Write as df for exploration
+#df = pd.DataFrame(data_list)
+#df.rename(columns={ df.columns[-1]: "folder/file" }, inplace = True)
+#df.to_csv("test_context_window_data.csv", index=False)
 
 ### Initialize and reformat as json ###
+df = pd.read_csv('test_context_window_data.csv')
+df = df.fillna('')
+df = df.drop('folder/file', 1)
 
-# Creates list of length N with document ids
-doc = list(chain(*([x]*(len(df.columns)-1) for x in range(len(df)))))
 
-# Create vocabulary and word tokens
-w = [word for sentence in df.drop("file", axis=1).values for word in sentence]
-vocab = set(w)-{'NA'}
-vocab = {token:idx+1 for idx, token in enumerate(vocab)}
-w = [vocab[token] -1 if str(token) != 'nan' else np.nan for token in w]
+# Extract document id and words to list format
+
+#doc,w = [word for sentence in enumerate(df) for word in sentence if word != '']
+#print(w)
+
+
+#w_counts = Counter({})
+#[w_counts.update(word) for word in df.values]
+#print(w_counts)
+#
+#doc,w,n = [],[],[]
+#for i, row in df.iterrows():
+#    words = row[row != '']
+#    w.extend(words)
+#    n.append(len(words))
+#    doc.extend([i]*len(words))
+#print(w)
+#
+#vocab = set(w)
+#vocab = {token:idx+1 for idx, token in enumerate(vocab)}
+#w = [vocab[token] -1 for token in w]
+
 V = len(vocab)
 M = max(doc)+1
-N = len([token for token in w if str(token) != 'nan']) # Excluding padded values
-
-# Hyperparams
-K = 5
-alpha = [0.5]*K
-beta = [0.5]*V
-
-# Initialize z
-z = (np.random.choice(K, M))
+N = len(w)
 
 data = {
-    "K": K,
     "V": V,
     "M": M,
     "N": N,
-    "alpha": alpha,
-    "beta": beta,
-    "z": z,
     "w": w,
-    "doc": doc
+    "doc": doc,
+    "n": n
 }
 
 # Make objects json compatible
@@ -115,11 +123,8 @@ class NpEncoder(json.JSONEncoder):
             return int(obj)
         elif isinstance(obj, np.floating):
             return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NpEncoder, self).default(obj)
-
-# Write file
 with open("test_context_window_data.json", "w") as outfile: 
     json.dump(data, outfile, cls=NpEncoder)
+
+with open("test_context_window_vocab.json", "w") as outfile: 
+    json.dump(vocab, outfile, cls=NpEncoder)
--- pyscripts/gibbs.py
+++ pyscripts/gibbs.py
@@ -1,85 +1,141 @@
+import enum
 import numpy as np
 import pandas as pd
 import os,json
 from collections import Counter
-from scipy.special import loggamma
 from progressbar import progressbar
+from itertools import dropwhile
+from scipy.special import logsumexp
+
+
+
 
 # Location of test_data
-path = '/home/robinsaberi/Git_Repos/concept-history/tests/test_data'
-files = ['test_stan_data.json','test_theta.json','test_phi.json']
-
+#path = '/home/robinsaberi/Git_Repos/concept-history/tests/test_data'
+#files = ['test_stan_data.json','test_theta.json','test_phi.json']
+#
 # Load files
-lst = []
-for i in range(len(files)):
-    with open(path+'/'+files[i]) as f:
-        lst.append(json.load(f))
+#lst = []
+#for i in range(len(files)):
+#    with open(path+'/'+files[i]) as f:
+#        lst.append(json.load(f))
+#
+#data,theta_true,phi_true = lst
+#theta_true = np.array((1,2,3,4)) / (1+2+3+4) # Bug in git data, enter manually until fixed
+#phi_true = np.array(phi_true)
+#K,V,M,N,alpha,beta,z_true,w,doc = list(data.values())
+#
+#z_true = [x-1 for x in z_true] # Index from 0
 
-data,theta_true,phi_true = lst
-theta_true = np.array((1,2,3,4)) / (1+2+3+4) # Bug in git data, enter manually until fixed
-phi_true = np.array(phi_true)
-K,V,M,N,alpha,beta,z_true,w,doc = list(data.values())
+# Riksdagen data
+path = '/home/robinsaberi/Git_Repos/concept-history/pyscripts/test_context_window_data.json'
+with open(path) as f:
+    data = json.load(f)# .values()
+    w,file,n = data.values()
 
-# Create df, make index go from 0
-z_true = [x-1 for x in z_true] # Index from 0
-df = pd.DataFrame(np.array((doc,w)).T) - 1
-df.columns = ["doc", "w"]
-df = df.groupby('doc')['w'].apply(list).reset_index()
+# Remove rare words
+c = Counter(x for xs in w for x in set(xs))
+rare_words = set([key for key, value in c.items() if value < 1000000000000])
 
-# Make these more general later
-df["z"] = np.random.choice(K, M)
-df["n"] = [int(N/M)]*M
+for lst in w:
+    for i, word in enumerate(lst):
+        if word in rare_words:
+            del lst[i]
+    print(lst)
+    if len(lst) == 0:
+        print(lst)
+        print('oops')
+        
+# Check that no document is completely empty
 
-# Create sufficient statistic matrices
-Nd = np.zeros(K, dtype = 'int')
-Nk = np.zeros((K,V), dtype = 'int')
-
-for doc in range(len(df)):
-    d,w,z,n = df.loc[doc]
-    Nd[z] += 1
-    np.add.at(Nk[z], w, 1)
-
-# Initiate theta,phi as unbiased sample estimates
-theta = Nd / Nd.sum()
-phi = Nk / Nk.sum(axis=1)[:, np.newaxis]
-
-# Run algo
-epochs = 50
-
-for epoch in progressbar(range(epochs)):
-    for doc in range(len(df)):
-        d,w,z,n = df.loc[doc]
-
-        # Get word counts for document
-        w_count = Counter(w)
-        counts = list(w_count.values())
-        keys = list(w_count.keys())
-
-        # Decrement counts
-        np.subtract.at(Nk[z], keys, counts)
-        Nd[z] -= 1
-
-        # Sample sense
-        probs = theta.copy()
-        for i in w:
-            probs *= phi[:,i]
-        probs = probs / probs.sum()
-
-        z = np.random.choice(K, p = probs)
-        df.loc[d,"z"] = z
-
-        # Increment counts
-        np.add.at(Nk[z], keys, counts)
-        Nd[z] += 1
-
-        # Sample new phi and theta
-        dir_beta = [np.random.dirichlet(row + beta) for row in Nk]
-        phi = np.array([i for i in dir_beta])
-        theta = np.array(np.random.dirichlet(Nd + alpha))
-
-# To do:
-# Add evaluation metric (that is comparable across different models)
-# Also make code a bit safer/general
-# Algo converges almost immediately and is then stuck, seems to be a case of multimodality and large influence from taking many phi products
+#K = 10
+#alpha,beta = 0.5,0.5
+#beta = 0.01
+#
+## Create df
+#df = pd.DataFrame(np.array((doc,w)).T)
+#df.columns = ["doc", "w"]
+#
+#wcount = Counter(w)
+#print(len(wcount))
+#for key, count in dropwhile(lambda key_count: key_count[1] >= 5, wcount.most_common()):
+#    del wcount[key]
+#print(len(wcount))
+#
+#print(2229 / 11678)
+#
+#df = df.groupby('w').filter(lambda x : (x['w'].count()>=5).any()) # somethings wrong ...
+#
+#
+#df = df.groupby('doc')['w'].apply(list).reset_index()
 
 
+
+# Test data
+#df["z"] = np.random.choice(K, M) - 1
+#df["n"] = [int(N/M)]*M
+
+# Riksdagen data
+#df["z"] = np.random.choice(K, M) - 1
+#df["n"] = n
+#
+## Create sufficient statistic matrices
+#Nd = np.zeros(K, dtype = 'int')
+#Nk = np.zeros((K,V), dtype = 'int')
+#
+#for doc in range(len(df)):
+#    d,w,z,n = df.loc[doc]
+#    Nd[z] += 1
+#    np.add.at(Nk[z], w, 1)
+#
+## Initiate theta,phi as unbiased sample estimates
+#theta = (Nd + alpha) / (Nd + alpha).sum()
+#phi = (Nk + beta) / (Nk + beta).sum(axis=1)[:, np.newaxis]
+#
+## Run algo
+#epochs = 50
+#
+#for epoch in progressbar(range(epochs)):
+#    for doc in range(len(df)):
+#        d,w,z,n = df.loc[doc]
+#
+#        # Get word counts for document
+#        w_count = Counter(w)
+#        counts = list(w_count.values())
+#        keys = list(w_count.keys())
+#
+#        # Decrement counts
+#        np.subtract.at(Nk[z], keys, counts)
+#        Nd[z] -= 1
+#
+#        # Sample sense
+#        log_probs = np.log(theta.copy())
+#        
+#
+#        
+#        for i in w:
+#            log_probs += np.log(phi[:,i])
+#        print(doc)
+#        print(np.min(phi))
+#
+#                
+#        z = np.random.choice(K, p = np.exp(log_probs - logsumexp(log_probs))) # make them sample from logits
+#        
+#        df.loc[d,"z"] = z
+#
+#        # Increment counts
+#        np.add.at(Nk[z], keys, counts)
+#        Nd[z] += 1
+#
+#        # Sample new phi and theta
+#        dir_beta = [np.random.dirichlet(row + beta) for row in Nk]
+#        phi = np.array([i for i in dir_beta])
+#        theta = np.array(np.random.dirichlet(Nd + alpha))
+#
+## To do:
+## Add evaluation metric (that is comparable across different models)
+## Also make code a bit safer/general
+## Algo converges almost immediately and is then stuck, seems to be a case of multimodality and large influence from taking many phi products
+#
+#
+#
