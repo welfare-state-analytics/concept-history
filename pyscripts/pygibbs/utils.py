d19d462833422748379991b07d391ff5981ff4a4
Robin Saberi <robin.saberinasseri@gmail.com>
2021-10-10 16:00:55 +0200

    added more post processing functions

--- pyscripts/pygibbs/utils.py
+++ pyscripts/pygibbs/utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from collections import Counter
 from bidict import bidict
+from sklearn.preprocessing import Normalizer
+import matplotlib.pyplot as plt
 
 def rareWords(doc, w, thresh=5):
     """
@@ -44,7 +46,7 @@ def initDataframe(doc, w, M):
 
 def distinctWords(Nk, beta, vocab, n=10):
     """
-    Computes top n p(z=k|w)
+    Computes top n p(z|w)
     """
     vocab_rev = bidict(vocab).inv
 
@@ -57,3 +59,44 @@ def distinctWords(Nk, beta, vocab, n=10):
         word_list.append([vocab_rev.get(key) for key in indices])
 
     return np.array(word_list).T
+
+def topWords(phi, vocab, n=10):
+    """
+    Computes top n p(w|K=k)
+    """
+    vocab_rev = bidict(vocab).inv
+
+    K = len(Nk)
+    word_list = []
+
+    for k in range(K):
+        indices = (-phi[k]).argsort()[:n]
+        word_list.append([vocab_rev.get(key) for key in indices])
+
+    return np.array(word_list).T
+
+def time_sense_plot(time, z, M, K, relative=False):
+    """
+    Plots senses over time from integer lists of inputs.
+    Relative normalizes sense counts to proportions in each given timeframe.
+    """
+    time_unique = sorted(list(set(time)))
+
+    # Crosstabulate sense by year
+    df = pd.DataFrame(np.zeros(shape=(len(time_unique), K), dtype=np.int32),\
+                               columns=list(range(K)), index=time_unique)
+    for m in range(M):
+        df.loc[time[m], z[m]] += 1
+
+    if relative == True:
+        df = Normalizer(norm='l1').fit_transform(df)
+
+    # Plot attributes can further be modified from return object
+    fig, ax = plt.subplots()
+    plt.plot(df)
+    plt.xlabel('time')
+    plt.ylabel('proportion' if relative else 'frequency')
+    plt.yticks(np.arange(0, 1, 0.2))
+    plt.legend([f'sense:{k}' for k in range(K)])
+    ax.spines[["top","right"]].set_visible(False)
+    return fig
