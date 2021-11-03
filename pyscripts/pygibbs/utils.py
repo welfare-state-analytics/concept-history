6396a5e4d7cb9b80b5b847755f1976662fdd143b
Robin Saberi <robin.saberinasseri@gmail.com>
2021-11-03 15:00:51 +0100

    most recent version of pygibbs module

--- pyscripts/pygibbs/utils.py
+++ pyscripts/pygibbs/utils.py
@@ -4,6 +4,7 @@ from collections import Counter
 from bidict import bidict
 from sklearn.preprocessing import Normalizer
 import matplotlib.pyplot as plt
+import seaborn as sns
 import subprocess
 
 def rareWords(doc, w, thresh=5):
@@ -13,7 +14,6 @@ def rareWords(doc, w, thresh=5):
     if len(doc) != len(w):
         print('Documents and word tokens are not of equal length')
         return
-
     c = Counter(w)
     rare_words = set([key for key, value in c.items() if value < thresh])
 
@@ -21,7 +21,6 @@ def rareWords(doc, w, thresh=5):
     for i in range(len(w)):
         if w[i] not in rare_words:
             idx.append(i)
-
     w = [w[i] for i in idx]
     doc = [doc[i] for i in idx]
 
@@ -45,18 +44,14 @@ def initDataframe(doc, w, M):
     df = df.groupby('doc')['w'].apply(list).reset_index()
     return df
 
-def distinctWords(Nk, vocab, n=20):
+def distinctWords(Nk, vocab, K, n=20, beta=0):
     """
     Computes top n p(z|w)
+    beta is set to 0 if hyperparameter is symmetric (results are proportional.)
     """
     vocab_rev = bidict(vocab).inv
-
-    K = len(Nk)
     word_list = []
-
-    # results = (Nk+beta) / (Nk+beta).sum(axis=0)[np.newaxis]
-    # Can exclude beta from equation as it does not contribute to the order of distinct words
-    results = Nk / Nk.sum(axis=0)[np.newaxis]
+    results = (Nk + beta) / (Nk + beta).sum(axis=0)[np.newaxis]
 
     for k in range(K):
         indices = (-results[k]).argsort()[:n]
@@ -67,112 +62,75 @@ def distinctWords(Nk, vocab, n=20):
 
     return distinctwords
 
-def topWords(phi, vocab, stopwords=False, n=20):
+def topWords(phi, vocab, K, stopwords=False, n=20):
     """
     Computes top n p(w|K=k)
     :param stopwords: list of stopwords   
     """
     if stopwords != False:
-        idx = [value for value in [vocab.get(word) for word in stopwords] if value is not None]
+        idx = [i for i in [vocab.get(word) for word in stopwords] if i is not None]
         phi = np.delete(phi, idx, axis=1)
-
     vocab_rev = bidict(vocab).inv
-
-    K = len(phi)
     word_list = []
-
+    
     for k in range(K):
         indices = (-phi[k]).argsort()[:n]
         word_list.append([vocab_rev.get(key) for key in indices])
-
     topwords = pd.DataFrame(np.array(word_list).T)
     topwords.columns = list(range(K))
 
     return topwords
 
-def time_sense_plot_old(time, z, M, K, relative=False):
-    """
-    Depreciated version used for test data.
-    Plots senses over time from integer lists of inputs.
-    Relative normalizes sense counts to proportions in each given timeframe.
-    """
-    time_unique = sorted(list(set(time)))
-
-    # Crosstabulate sense by year
-    df = pd.DataFrame(np.zeros(shape=(len(time_unique), K), dtype=np.int32),\
-                               columns=list(range(K)), index=time_unique)
-    for m in range(M):
-        df.loc[time[m], z[m]] += 1
-
-    if relative == True:
-        df = Normalizer(norm='l1').fit_transform(df)
-
-    # Plot attributes can further be modified from return object
-    fig, ax = plt.subplots()
-    plt.plot(df)
-    plt.xlabel('time')
-    plt.ylabel('proportion' if relative else 'frequency')
-    plt.yticks(np.arange(0, 1, 0.2))
-    plt.legend([f'sense:{k}' for k in range(K)])
-    ax.spines[["top","right"]].set_visible(False)
-    return fig, ax
-
-def senses_over_time(time, doc, z, K, relative=True):
+def senses_over_time(time, doc, z, K, color, relative=True):
     """
     Plots senses over time from integer lists of inputs.
     Relative normalizes sense counts to proportions in each given timeframe.
     """
-    z_long = [z[doc[i]] for i in range(len(doc))]
+    z_long = [z[m] for m in doc]
     df = pd.DataFrame({'time': time, 'z': z_long})
-    df = pd.crosstab(df["time"], df["z"])
-
-    if relative == True:
-        df = df.div(df.sum(axis=1), axis=0)
+    df = pd.crosstab(df["time"], df["z"], normalize='index' if relative else False)
+    df["time"] = df.index
+    df = pd.melt(df, 'time')
 
     fig, ax = plt.subplots()
-    plt.plot(sorted(set(time)), df)
     plt.xlabel('time')
     plt.ylabel('proportion' if relative else 'frequency')
-    if relative == True:
-        plt.yticks(np.arange(0, 1.01, 0.2))
+    if relative:
+        plt.yticks(np.arange(0, 1, 0.2))
     plt.legend([f'sense:{k}' for k in range(K)])
     ax.spines[["top","right"]].set_visible(False)
+
+    pal = sns.color_palette(color)
+    palmap = dict_variable = {key:value for (key,value) in zip(list(range(K)), pal)}
+    ax = sns.lineplot(x=df['time'], y=df['value'], hue=df['z'], palette=palmap)
 
     return (fig, ax)
 
-def word_freq_over_time(time, target, relative=True):
+def word_freq_over_time(time, target, color, relative=True):
     """
     Plots word freqs over time from integer lists of inputs.
     Target word list should be lematized.
     """
-    #if len(set(target)) == 1:
-    #    target = list(target)
+    df = pd.DataFrame({'time': time, 'target': target})
+    df = pd.crosstab(df["time"], df["target"])
 
-    df = pd.DataFrame({'time': time, 'freq': target})
-    df = pd.crosstab(df["time"], df["freq"])
+    lemmas = sorted(list(set(target)))
+    legend = []
+    fig, ax = plt.subplots()
 
     if relative == True:
         df = df.div(df.sum(axis=1), axis=0)
-
-    fig, ax = plt.subplots()
-    plt.plot(sorted(set(time)), df)
-    plt.xlabel('time')
-    plt.ylabel('frequency')
-    if relative == True:
         plt.yticks(np.arange(0, 1.01, 0.2))
 
-    plt.legend([f'word:{v}' for v in sorted(list(set(target)))])
+    for i,lemma in enumerate(lemmas):
+        plt.plot(df.index, df[lemma], color=color[i])
+        legend.append(f'lemma: {lemma}')
+    plt.legend(legend)
+    plt.xlabel('time')
+    plt.ylabel('proportion' if relative else 'frequency')
     ax.spines[["top","right"]].set_visible(False)
 
     return (fig, ax)
-
-def z_sorter_old(z, K):
-    """
-    Sorts z in descending frequency order for easier comparisons between runs and models.
-    """
-    z_sorted = [count[0] for count in Counter(z).most_common()]
-    z_new = list(range(K))
-    return [z_new[z_sorted.index(zi)] for zi in z]
 
 def z_sorter(z, phi, Nk, K):
     """
@@ -180,13 +138,9 @@ def z_sorter(z, phi, Nk, K):
     """
     z_sorted = [count[0] for count in Counter(z).most_common()]
     z_list = list(range(K))
-    z = [z_list[z_sorted.index(zi)] for zi in z]
+    z_sorted.extend(z_list[len(z_sorted)-K:]) # adds back empty topics
 
-    # Sort phi, Nk accordingly
-    #idx = np.empty_like(z_sorted)
-    #idx[z_sorted] = np.arange(len(z_sorted))
-    #phi = phi[:, idx]
-    #Nk = Nk[:, idx]
+    z = [z_list[z_sorted.index(zi)] for zi in z]
     phi = phi[z_sorted]
     Nk = Nk[z_sorted]
     return (z, phi, Nk)
@@ -200,3 +154,5 @@ def git_auto_push(path, message):
     subprocess.call(f'git add {path}', shell=True)
     subprocess.call(f'git commit -m "{message}"', shell=True)
     subprocess.call('git push', shell=True)
+
+    
